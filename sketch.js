e4f7c7ac75c41423c7e6ba5f5d6db8da92fe2f76
shunsuke-tamura <wwwxx4869@gmail.com>
2022-08-13 22:23:53 +0900

    update: 棒倒し法で迷路生成

--- sketch.js
+++ sketch.js
@@ -1,10 +1,23 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
   background(31, 127, 255);
+  rect(50, 50, 500, 500);
+  for (let x = 100; x < 550; x += 50) {
+    for (let y = 100; y < 550; y += 50) {
+      ellipse(x, y, 5);
+      randomDrawLine(x, y);
+    }
+  }
 }
 
 function draw() {
-  noStroke();
-  fill(255, 255, 0);
-  circle(width / 2, height / 2, height / 2);
+
+}
+
+function randomDrawLine(startX, startY) {
+  const f = Math.floor(Math.random() * 4);
+  if (f === 0) { line(startX, startY, startX, startY - 50); }
+  else if (f === 1) { line(startX, startY, startX + 50, startY); }
+  else if (f === 2) { line(startX, startY, startX, startY + 50); }
+  else { line(startX, startY, startX - 50, startY); }
 }

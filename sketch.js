a943e8b9b504144d49d99e0427b72bb1ce7e01c8
shunsuke-tamura <wwwxx4869@gmail.com>
2022-08-14 11:11:50 +0900

    udpate: 迷路生成を軽く一般化

--- sketch.js
+++ sketch.js
@@ -1,23 +1,40 @@
+let flame = 0
+let maze
+
 function setup() {
-  createCanvas(600, 600);
-  background(31, 127, 255);
-  rect(50, 50, 500, 500);
-  for (let x = 100; x < 550; x += 50) {
-    for (let y = 100; y < 550; y += 50) {
-      ellipse(x, y, 5);
-      randomDrawLine(x, y);
-    }
-  }
+  createCanvas(600, 600)
+  background(31, 127, 255)
+  rect(50, 50, 500, 500)
 }
 
 function draw() {
-
+  if (flame % 120 === 0 && flame / 120 < 5) {
+    maze = new Maze((flame / 120 + 1) * 10)
+    maze.generateMaze()
+  }
+  flame += 1
 }
 
-function randomDrawLine(startX, startY) {
-  const f = Math.floor(Math.random() * 4);
-  if (f === 0) { line(startX, startY, startX, startY - 50); }
-  else if (f === 1) { line(startX, startY, startX + 50, startY); }
-  else if (f === 2) { line(startX, startY, startX, startY + 50); }
-  else { line(startX, startY, startX - 50, startY); }
+class Maze {
+  constructor(lineNum) {
+    this.lineNum = lineNum
+    clear()
+    createCanvas(600, 600)
+    background(31, 127, 255)
+    rect(50, 50, 500, 500)
+  }
+
+  // 棒倒し法で迷路生成
+  generateMaze() {
+    const increase = 500 / this.lineNum
+    for (let y = 50 + increase; y < 550; y += increase) {
+      for (let x = 50 + increase; x < 550; x += increase) {
+        const f = Math.floor(Math.random() * 4)
+        if (f === 0) { line(x, y, x, y - increase) }
+        else if (f === 1) { line(x, y, x + increase, y) }
+        else if (f === 2) { line(x, y, x, y + increase) }
+        else { line(x, y, x - increase, y) }
+      }
+    }
+  }
 }
